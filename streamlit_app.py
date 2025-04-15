13b4623c0f357b4eaee04a9d250caeea998d4a1c
Soujatya Das <soujatya548@gmail.com>
2025-04-15 15:05:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -263,7 +263,7 @@ def initialize_llm():
     
     llm = ChatGroq(
         api_key=groq_api_key,
-        model_name="llama3-70b-8192"
+        model_name="llama-4-scout-17b-16e-instruct"
     )
     
     retriever = st.session_state.vector_store.as_retriever(
