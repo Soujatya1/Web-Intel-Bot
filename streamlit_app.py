f47cc2f559fa987ed1b1ce5cbb8f645ee76b647d
Soujatya Das <soujatya548@gmail.com>
2025-07-30 04:14:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ from urllib.parse import urljoin, urlparse
 from collections import Counter
 
 HARDCODED_WEBSITES = ["https://irdai.gov.in/acts",
-                      "https://enforcementdirectorate.gov.in/pmla"
+                      "https://enforcementdirectorate.gov.in/pmla",
+                      "https://uidai.gov.in/en/about-uidai/legal-framework/notification.html"
                      ]
 
 def smart_document_filter(document_links, query, ai_response, max_docs=3):
